17a76851982d9b015029b75816e07a3162d23022
Vaishnavi Patil <57519551+vaishnavi477@users.noreply.github.com>
2025-10-29 17:04:40 -0700

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     key    = "github-actions-cicd/terraform.tfstate" # the directory/file.tfstate
-    bucket = "ADD_YOUR_BUCKET_NAME_HERE"             # the bucket
-    region = "ADD_YOUR_REGION_NAME_HERE"             # the region
+    bucket = "infrastructure-as-a-code-s3-bucket"             # the bucket
+    region = "us-east-1"                                      # the region
   }
 }
